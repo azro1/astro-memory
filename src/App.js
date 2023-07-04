8b6d0e39544cf1f12ca0ed19b597a8bf1416d68d
Azro1 <azro1.development@gmail.com>
2023-07-04 20:04:05 +0100

    shuffling cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { src: '/images/astro1.png' },
+  { src: '/images/astro2.png' },
+  { src: '/images/astro3.png' },
+  { src: '/images/astro4.png' },
+  { src: '/images/astro5.png' },
+  { src: '/images/astro6.png' },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shufffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      // sort fires a function for each pair of items in an array - if we return a number less than 0 the order of those 2 items stays the same, if we return a number greater than 0 then the order of those 2 items that it's comparing is swapped - to get a random order Math.random() - 0.5 returns both negative and positive numbers
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
-    <div className="App">
-       <h1>Astro Match</h1>
-       <button>New Game</button>
+    <div className='App'>
+      <h1>Astro Match</h1>
+      <button onClick={shufffleCards} >New Game</button>
     </div>
   );
 }
